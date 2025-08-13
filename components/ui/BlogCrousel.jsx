79f9beaa9b4b7f7f888cea96938a9fd4d0aaad6f
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-13 15:46:16 +0500

    Ui bug fixed

--- components/ui/BlogCrousel.jsx
+++ components/ui/BlogCrousel.jsx
@@ -55,10 +55,17 @@ export default function BlogCarousel({ blogs }) {
             />
         ),
         responsive: [
-            { breakpoint: 1024, settings: { slidesToShow: 2 } },
-            { breakpoint: 640, settings: { slidesToShow: 1 } },
+            {
+                breakpoint: 1024,
+                settings: { slidesToShow: 2 }
+            },
+            {
+                breakpoint: 640,
+                settings: { slidesToShow: 1 }
+            }
         ],
     };
+
 
     return (
         <Slider {...settings}>
