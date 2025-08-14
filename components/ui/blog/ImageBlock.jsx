41ca8142b5f4b2f77617e3b474898b0e7b789594
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-14 23:17:38 +0500

    Resolved minro bug

--- components/ui/blog/ImageBlock.jsx
+++ components/ui/blog/ImageBlock.jsx
@@ -1,17 +1,15 @@
 import Image from 'next/image';
 import React from 'react'
 
-const ImageBlock = ({item}) => {
-  return (
-        <div className={item.styles?.join(" ")}>
-            <Image
-                src={item.src}
-                alt={item.alt || ""}
-                width={800}
-                height={400}
-                className="w-full h-auto"
-            />
-        </div>
+const ImageBlock = ({ item }) => {
+    return (
+        <Image
+            src={item.src}
+            alt={item.alt || ""}
+            width={800}
+            height={400}
+            className={item.styles?.join(" ")}
+        />
     );
 }
 
